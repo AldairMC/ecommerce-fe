15e3f1ac5a8c003e12101f5075ce7e70efa904bd
AldairMC <aldairmc2023@gmail.com>
2020-08-25 14:30:20 -0500

    :art: Modifying files core

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+//Components
+import Router from '../Router'
+
+const App = () => {
   return (
-    <div >
-      <h1>Init project E-COMMERCE</h1>
+    <div>
+      <Router />
     </div>
   );
 }
